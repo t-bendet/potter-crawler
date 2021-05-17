7d5fce04041d719ecc4f5d17e99418864fabb6c8
t-bendet <talbendet21@gmail.com>
2021-05-17 14:46:18 +0300

    minor edit

--- crawler/index.js
+++ crawler/index.js
@@ -17,7 +17,7 @@ const browserObject = require("./browser");
 // const scraperController = require("../spells/pageController");
 
 //Start the browser and create a browser instance
-let browserInstance = browserObject.startBrowser();
+// let browserInstance = browserObject.startBrowser();
 
 // Pass the browser instance to the scraper controller
 scraperController(browserInstance);
--- src/index.js
+++ src/index.js
@@ -18,6 +18,9 @@ app.use(charactersRoute);
 app.use(spellsRoute);
 app.use(potionsRoute);
 app.use(creaturesRoute);
+app.get("/", (req, res) => {
+  res.send("it's kind of boring here,check out the routes");
+});
 
 app.listen(port, () => {
   console.log("Server is up on port " + port);
